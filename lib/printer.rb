95bb85a076d0a3c1fbbbdbc5d8836b6da78b9026
Squareben1 <bengittins@live.com>
2020-02-26 11:56:13 +0000

    print_balance method

--- lib/printer.rb
+++ lib/printer.rb
@@ -8,15 +8,10 @@ class Printer
 
   def print_balance(transactions)
     string = "date || credit || debit || balance\n"
-    # transaction_strings = ''
-    # if @transactions.empty?
-    #   string
-    # else
-    #   @transactions.each do |transaction|
-    #     @new_string = string + "\n#{transaction.date} || #{transaction.type} || #{transaction.amount} || #{transaction.balance}"
-    #   end
-    #   @new_string
-    #   # string + "\n24/02/2020 || credit || 10 || 10"
-    # end
+    if transactions[0] == ''
+      string
+    else
+      string + transactions.join("\n")
+    end
   end
 end 
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,14 +2,27 @@ require 'printer'
 
 describe Printer do
   describe '#print_balance' do
-    it 'should return headings only when no transactions present' do
-      expect(subject.print_balance('')).to eq "date || credit || debit || balance\n"
+    it 'should return heading only when no transactions present' do
+      expect(subject.print_balance([''])).to eq "date || credit || debit || balance\n"
     end
 
-    it 'should return headings and 1 deposit as string w attrs seperated by ||' do
-      expect(subject.print_balance('24/02/2020 || credit || 10 || 10')).to eq "date || credit || debit || balance\n24/02/2020 || credit || 10 || 10\n"
+    it 'should return heading and 1 deposit as string w attrs seperated by ||' do
+      expect(subject.print_balance(['24/02/2020 || credit || 10 || 10'])).to eq "date || credit || debit || balance\n24/02/2020 || credit || 10 || 10"
     end
 
+    it 'should return heading and 1 deposit as string w attrs seperated by ||' do
+      expect(subject.print_balance(['24/02/2020 || credit || 10 || 10', '24/02/2020 || credit || 10 || 10'])).to eq "date || credit || debit || balance\n24/02/2020 || credit || 10 || 10\n24/02/2020 || credit || 10 || 10"
+    end
 
+    # xit 'should iterate over multiple transactions in array and print correctly' do
+    #   transaction_class = class_double('Transaction')
+    #   transaction_instance = instance_double('Transaction', { date: '24/02/2020', type: 'credit', amount: 10, balance: 10 })
+    #   allow(transaction_class).to receive(:new).and_return(transaction_instance)
+    #   subject = Account.new(0, transaction_class)
+    #   subject.deposit(10)
+    #   subject.deposit(10)
+    #   p subject.transactions[0].amount
+    #   expect(subject.print_balance).to eq "date || credit || debit || balance\n24/02/2020 || credit || 10 || 10\n24/02/2020 || credit || 10 || 10"
+    # end
   end
 end
