6ba668f62911e72a2b98a95ed29b6b8e9ed81729
Squareben1 <bengittins@live.com>
2020-02-26 12:43:22 +0000

    program working

--- spec/features/user_can_print_statements_spec.rb
+++ spec/features/user_can_print_statements_spec.rb
@@ -4,7 +4,7 @@ require 'printer'
 require 'account'
 
 describe Printer do
-  it 'pulls transactions from account and prints them' do
+  it 'prints single transaction from account' do
     account = Account.new
     account.deposit(10)
     printer = Printer.new
@@ -12,6 +12,14 @@ describe Printer do
     # p "stringify: #{printer.stringify_transactions(account.transactions)}"
     # p "print: #{printer.print_balance(printer.stringify_transactions(account.transactions))}"
     expect(printer.print_balance(printer.stringify_transactions(account.transactions))).to eq "date || credit || debit || balance\n26/02/2020 || 10 || || 10"
-  end 
-end 
+  end
+
+  it 'prints two transactions from account, a credit and a debit' do
+    account = Account.new
+    account.deposit(10)
+    account.withdraw(10)
+    printer = Printer.new
+    expect(printer.print_balance(printer.stringify_transactions(account.transactions))).to eq "date || credit || debit || balance\n26/02/2020 || 10 || || 10\n26/02/2020 || || 10 || 0"
+  end
+end
 
